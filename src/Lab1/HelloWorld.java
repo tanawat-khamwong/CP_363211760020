c0115c8f071ee73762e50266b29adfa5ba592c68
bank <tanawat.k@rmutsvmail.com>
2020-07-14 17:54:42 +0700

    add code 2lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HolloWorld !!!");
         System.out.println("Tanawat");
+        System.out.println("Bank");
 
     }
 }
