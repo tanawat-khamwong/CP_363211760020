9c20faf2343027c9c26256db67f7a021068a3e2b
bank <tanawat.k@rmutsvmail.com>
2020-07-14 17:41:08 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HolloWorld !!!");
+        System.out.println("Tanawat");
+
+    }
+}
