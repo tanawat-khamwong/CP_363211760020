4e753273025e0d3872e00049a76d409077b04cf1
Bank <tanawat.k@rmutsvmail.com>
2020-09-26 12:24:37 +0700

    Lab 4:Login1

---
+++ src/Lab5/BasicArray2D.java
@@ -0,0 +1,39 @@
+package Lab5;
+
+import java.util.Scanner;
+
+public class BasicArray2D {
+    public static void main(String[]args){
+        int number[][]= new int[2][3]; //2*3=6 blocks
+
+        //input Data to array 2D
+        number =inputData(number);
+        //display data in array 2D
+        number =displayData(number);
+    }
+
+    private static int[][] displayData(int[][] number) {
+        System.out.println("Data in array 2D:");
+        for (int i = 0; i < number.length; i++) {             //row
+            for (int j = 0; j < number[i].length; j++) {      //column
+                System.out.println(number[i][j]+" ");  //number[0][0]:
+            }
+            System.out.println();
+        }
+        private static int[][] inputData(int[][] number){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please,input integers to array 2D:");
+            for (int i = 0; i < number.length; i++) {             //row
+                for (int j = 0; j < number[i].length; j++) {      //column
+                    System.out.println("number[" + i + "][" + j + "]:");  //number
+                    number[i][j] = sc.nextInt();
+                }
+
+        }
+
+
+
+
+
+
+
